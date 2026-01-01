2c8b2584c45ead4f131777cbda5ff848e920b00a
Vibhu Rathore <rathorevibhu10@gmail.com>
2026-01-01 18:09:21 +0530

    feat : sidebar almost complete

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Sidebar from '@/components/Sidebar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className="min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10">
+      <Sidebar/>
       <App />
     </div>
   </StrictMode>,
