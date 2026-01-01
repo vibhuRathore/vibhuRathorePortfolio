f68b5562bbb24d4be6dbb25aee4f8d5a920da617
Vibhu Rathore <rathorevibhu10@gmail.com>
2026-01-01 23:03:54 +0530

    feat : floating menu completed

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Sidebar from '@/components/Sidebar.tsx'
+import FloatingMenu from './components/FloatingMenu.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className="min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10">
+      <FloatingMenu/>
       <Sidebar/>
       <App />
     </div>
