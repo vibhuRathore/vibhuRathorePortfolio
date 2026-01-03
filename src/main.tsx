dfa3990cac178124cab963aa8bbd138d67bd64a8
Vibhu Rathore <rathorevibhu10@gmail.com>
2026-01-03 14:46:07 +0530

    feat : profile card complete

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import Sidebar from '@/components/Sidebar.tsx'
 import FloatingMenu from './components/FloatingMenu.tsx'
+import Profile from './components/Profile.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className="min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10">
       <FloatingMenu/>
       <Sidebar/>
+      <Profile />
       <App />
     </div>
   </StrictMode>,
